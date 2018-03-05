b584d44adc8bb5b38c2b707f6a65b6f5437e0114
likang <lightning-li@foxmail.com>
2018-03-05 20:37:09 +0800

    fix

--- src/zcash/random.h
+++ src/zcash/random.h
@@ -18,10 +18,19 @@ int GetRandInt(int nMax);
 // [mapFirst, mapFirst+len) 根据上述元素的排列进行相同的排列，便于调用者追踪排列
 // gen 使用整数 n 作为参数，并且产生一个位于 [0,n) 随机的输出
 
-template<typename RandomAccessIterator, typename MapRandomAccessIterator>
+template <typename RandomAccessIterator, typename MapRandomAccessIterator>
 void MappedShuffle(RandomAccessIterator first,
                    MapRandomAccessIterator mapFirst,
                    size_t len,
-                   std::function<int(int)> gen);
+                   std::function<int(int)> gen)
+{
+    for (size_t i = len-1; i > 0; --i) {
+        auto r = gen(i+1);
+        assert(r >= 0);
+        assert(r <= i);
+        std::swap(first[i], first[r]);
+        std::swap(mapFirst[i], mapFirst[r]);
+    }
+}
 
 #endif
