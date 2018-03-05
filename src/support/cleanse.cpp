d53c37ca2608bd72235849c702f51ed41f981dd3
likang <lightning-li@foxmail.com>
2018-03-05 21:51:32 +0800

    add OPENSSL_cleanse

--- src/support/cleanse.cpp
+++ src/support/cleanse.cpp
@@ -4,8 +4,17 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "support/cleanse.h"
+#include <string.h>
 
-#include <openssl/crypto.h>
+//#include <openssl/crypto.h>
+// learned from openssl/crypto/mem_clr.c
+
+typedef void *(*memset_t)(void *, int, size);
+static volatile memset_t memset_func = memset;
+
+void OPENSSL_cleanse(void *ptr, size_t len) {
+    memset_func(ptr, 0, len);
+}
 
 void memory_cleanse(void *ptr, size_t len)
 {
